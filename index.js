ab4b1bd04ff31f5c0e1f92291d15ed70a9072794
Shehrozkhannn <sharozekkhan@gmail.com>
2024-11-25 00:34:33 +0500

    practice params and query params

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const server = http.createServer((req,res)=> {
-  const log = `${Date.now()}: ${req.url}  new request recieved\n`
+  const log = `${Date.now()}: ${req.url}  new request recieved\n`;
+  const myUrl = url.parse(req.url);
+  console.log(myUrl)
   fs.appendFile('log.txt',log,(err,data)=>{
     res.end('Hello from server')
   })
