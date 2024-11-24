6a484c98055edd5b845e6077d2c25bf086e87117
Shehrozkhannn <sharozekkhan@gmail.com>
2024-11-24 19:12:24 +0500

    solve hacker rank breaking record problem

--- hacker-rank/test.js
+++ hacker-rank/test.js
@@ -24,4 +24,30 @@
 //   return `${hours.toString().padStart(2,"0")}:${mins.toString().padStart(2,"0")}:${secs.toString().padStart(2,"0")}`
 // }
 
-console.log(timeConversion('06:01:00PM'));
+// console.log(timeConversion('06:01:00PM'));
+
+
+/// BREAKING RECORDS PROBLEM /////////////
+
+const scores = [12,24,10,24];
+let prevMin = scores[0];
+let prevMax = scores[0];
+let minCount = 0;
+let maxCount = 0;
+const breakingRecords = (scores) => {
+  console.log('Game' , "Score" , "Min" , "max" , 'count(min)', 'count(max)')    
+  for (let index = 0; index < scores.length; index++) {
+    const score = scores[index];
+    if(score > prevMax){
+      maxCount += 1
+      prevMax = scores[index]
+    }else if (score < prevMin){
+      minCount += 1;
+      prevMin = scores[index]
+    }
+    console.log(
+      `${index.toString().padEnd(5)}  ${score.toString().padEnd(6)}  ${prevMin.toString().padEnd(5)}  ${prevMax.toString().padEnd(5)}  ${minCount.toString().padEnd(12)}  ${maxCount}`
+    );
+  }
+}
+breakingRecords(scores);
